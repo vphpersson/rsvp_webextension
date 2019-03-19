fcb928e8e5544ed19bba6a82e5ed6c1214fa990d
vph <vph.persson@gmail.com>
2019-03-19 23:03:39 +0100

    Added controls.

--- js/background.js
+++ js/background.js
@@ -4,7 +4,10 @@ const CONFIG = {
     rsvp_html_file_path: '/components/rsvp.html',
     rsvp_js_file_path: '/js/rsvp.js',
     word_element_tag_name: 'span',
-    word_display_element_selector: '.word_display'
+    word_display_element_selector: '.word_display',
+    words_per_minute: 400,
+    word_color: '#FF0000',
+    done_text: 'DONE!'
 };
 
 async function handle_message(message) {
@@ -22,6 +25,9 @@ async function handle_message(message) {
                 text_container_selector: CONFIG.text_container_selector,
                 word_display_element_selector: CONFIG.word_display_element_selector,
                 word_element_tag_name: CONFIG.word_element_tag_name,
+                words_per_minute: CONFIG.words_per_minute,
+                word_color: CONFIG.word_color,
+                done_text: CONFIG.done_text,
                 text: message.text,
             });
 
