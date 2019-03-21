1a04a9646f10507d307018d7b5da23dac7a6f4b2
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2019-03-21 23:54:39 +0100

    Cleaned up rsvp.

--- js/background.js
+++ js/background.js
@@ -3,59 +3,52 @@ const CONFIG = {
     text_container_selector: '.main',
     rsvp_html_file_path: '/components/rsvp.html',
     rsvp_js_file_path: '/js/rsvp.js',
+    content_select_element_js_file_path: '/js/content_select_element.js',
+    content_heuristic_select_element_js_file_path: '/js/content_heuristic_select_element.js',
     word_element_tag_name: 'span',
     word_display_element_selector: '.word_display',
+    topbar_selector: '.topbar',
+    word_color_input_selector: '.word_color_input',
+    wpm_input_selector: '.wpm_input',
     words_per_minute: 400,
     word_color: '#FF0000',
     done_text: 'DONE!'
 };
 
-// TODO: Add a `get_config` function.
-
-// TODO: I want no constants in here!
 async function handle_message(message) {
     const {message_type} = message;
 
     switch (message_type) {
+        case 'get_config': {
+            // TODO: Add lookups in `storage`.
+            return CONFIG;
+        }
         case 'receive_text': {
-            // NOTE: This code relies on `executeScript` returning a `Promise`. This does not appear to be supported by
-            // the webextension polyfill, and thus this web extension is _unusable in Chrome_.
+            const text = await (async () => {
+                const {mode} = message;
 
-            const {mode} = message;
+                // NOTE: This code relies on `executeScript` returning a `Promise`. This does not appear to be supported by
+                // the webextension polyfill, and thus this web extension is _unusable in Chrome_.
 
-            let text;
-            switch (mode) {
-                case 'heuristic_select_element': {
-                    [text] = await browser.tabs.executeScript({file: '/js/content_heuristic_select_element.js'});
-                    break;
+                switch (mode) {
+                    case 'heuristic_select_element': {
+                        return (await browser.tabs.executeScript({file: CONFIG.content_heuristic_select_element_js_file_path}))[0];
+                    }
+                    case 'select_element': {
+                        return (await browser.tabs.executeScript({file: CONFIG.content_select_element_js_file_path}))[0];
+                    }
+                    default: {
+                        throw Error(`Unsupported receive_text mode: ${mode}`);
+                    }
                 }
-                case 'select_element': {
-                    [text] = await browser.tabs.executeScript({file: '/js/content_select_element.js'});
-                    break;
-                }
-                default: {
-                    return void console.error(`Unsupported receive_text mode: ${mode}`);
-                }
-            }
+            })();
 
             if (text === null)
                 return void console.warn('The selected text is `null`.');
 
             const extension_tab = await browser.tabs.create({url: CONFIG.rsvp_html_file_path});
-
-            await browser.tabs.executeScript(extension_tab.id, {
-                file: CONFIG.rsvp_js_file_path
-            });
-
-            await browser.tabs.sendMessage(extension_tab.id, {
-                text_container_selector: CONFIG.text_container_selector,
-                word_display_element_selector: CONFIG.word_display_element_selector,
-                word_element_tag_name: CONFIG.word_element_tag_name,
-                words_per_minute: CONFIG.words_per_minute,
-                word_color: CONFIG.word_color,
-                done_text: CONFIG.done_text,
-                text,
-            });
+            await browser.tabs.executeScript(extension_tab.id, {file: CONFIG.rsvp_js_file_path});
+            await browser.tabs.sendMessage(extension_tab.id, {text});
 
             break;
         }
--- js/rsvp.js
+++ js/rsvp.js
@@ -1,181 +1,204 @@
-
-function* make_sibling_iterator(element) {
-    while (element !== null) {
-        yield element;
-        element = element.nextSibling;
-    }
-}
-
-class RsvpHandler {
-    constructor(word_display_element, display_time_ms = 150, word_color = 'red', done_text = 'DONE!') {
-        this.word_display_element = word_display_element;
-        this.word_elements_iterator = null;
-
-        this._display_time_ms = display_time_ms;
-        this._word_color = word_color;
-        this.done_text = done_text;
-
-        this._current_timeout_id = null;
-        this._previous_element = null;
-    }
-
-    get is_active() {
-        return this._current_timeout_id !== null;
-    }
-
-    get display_time_ms() {
-        return this._display_time_ms;
-    }
-
-    set display_time_ms(ms_time) {
-        // TODO: Fix the case where the delay is 0.
-        this._display_time_ms = ms_time === Infinity ? 0 : (ms_time || 0);
-    }
-
-    get word_color() {
-        return this._word_color;
-    }
-
-    set word_color(color) {
-        this._word_color = color;
-        this._color_word_element(this._previous_element);
-    }
-
-    set_display_text(text) {
-        this.word_display_element.textContent = text;
-    }
-
-    // TODO: Make static?
-    _uncolor_word_element(element) {
-        if (element) {
-            element.style.color = '';
+(() => {
+    function* make_sibling_iterator(element) {
+        while (element !== null) {
+            yield element;
+            element = element.nextSibling;
         }
     }
 
-    _color_word_element(element) {
-        if (element) {
-            element.style.color = this.word_color;
+    class RsvpHandler {
+        constructor(word_display_element, display_time_ms = 150, word_color = 'red', done_text = 'DONE!') {
+            this.word_display_element = word_display_element;
+            this.word_elements_iterator = null;
+
+            this._display_time_ms = display_time_ms;
+            this._word_color = word_color;
+            this.done_text = done_text;
+
+            this._current_timeout_id = null;
+            this._previous_element = null;
         }
-    }
 
-    _rewind_or_forward(direction_attribute_name, num_steps) {
-        // TODO: Make nicer?
-        const [iter_res, last_iter_element] = (() => {
-            let i, iter, iter_element;
-            for (i = 0, iter = make_sibling_iterator(this._previous_element), iter_element = this._previous_element; i < num_steps; i++) {
-                if (iter_element[direction_attribute_name] === null)
-                    return [iter,  iter_element];
+        get is_active() {
+            return this._current_timeout_id !== null;
+        }
 
-                iter_element = iter_element[direction_attribute_name];
-                iter = make_sibling_iterator(iter_element);
+        get display_time_ms() {
+            return this._display_time_ms;
+        }
+
+        set display_time_ms(ms_time) {
+            // TODO: Fix the case where the delay is 0.
+            this._display_time_ms = ms_time === Infinity ? 0 : (ms_time || 0);
+        }
+
+        get word_color() {
+            return this._word_color;
+        }
+
+        set word_color(color) {
+            this._word_color = color;
+            this._color_word_element(this._previous_element);
+        }
+
+        set_display_text(text) {
+            this.word_display_element.textContent = text;
+        }
+
+        // TODO: Make static?
+        _uncolor_word_element(element) {
+            if (element) {
+                element.style.color = '';
             }
-            return [iter, iter_element];
-        })();
+        }
 
-        this.word_elements_iterator = iter_res;
+        _color_word_element(element) {
+            if (element) {
+                element.style.color = this.word_color;
+            }
+        }
 
-        this._uncolor_word_element(this._previous_element);
-        this._color_word_element(last_iter_element);
-        this.set_display_text(last_iter_element.textContent);
+        _rewind_or_forward(direction_attribute_name, num_steps) {
+            // TODO: Make nicer?
+            const [iter_res, last_iter_element] = (() => {
+                let i, iter, iter_element;
+                for (i = 0, iter = make_sibling_iterator(this._previous_element), iter_element = this._previous_element; i < num_steps; i++) {
+                    if (iter_element[direction_attribute_name] === null)
+                        return [iter,  iter_element];
 
-        this._previous_element = last_iter_element;
+                    iter_element = iter_element[direction_attribute_name];
+                    iter = make_sibling_iterator(iter_element);
+                }
+                return [iter, iter_element];
+            })();
+
+            this.word_elements_iterator = iter_res;
+
+            this._uncolor_word_element(this._previous_element);
+            this._color_word_element(last_iter_element);
+            this.set_display_text(last_iter_element.textContent);
+
+            this._previous_element = last_iter_element;
+        }
+
+        rewind(num_steps = 8) {
+            return this._rewind_or_forward('previousElementSibling', num_steps);
+
+        }
+
+        forward(num_steps = 8) {
+            return this._rewind_or_forward('nextElementSibling', num_steps);
+        }
+
+        run() {
+            if (this.word_elements_iterator === null)
+                return void console.warn('The word elements iterator is `null`.');
+
+            const {value: iter_word_element, done} = this.word_elements_iterator.next();
+
+            this._uncolor_word_element(this._previous_element);
+
+            if (done)
+                return void this.set_display_text(this.done_text);
+
+            this._color_word_element(iter_word_element);
+            this.set_display_text(iter_word_element.textContent);
+
+            this._previous_element = iter_word_element;
+            // TODO: Fix the case where the delay is 0. Add an if statement?
+            this._current_timeout_id = window.setTimeout(this.run.bind(this), this.display_time_ms);
+        }
+
+        pause() {
+            window.clearTimeout(this._current_timeout_id);
+            this._current_timeout_id = null;
+        }
     }
 
-    rewind(num_steps = 8) {
-        return this._rewind_or_forward('previousElementSibling', num_steps);
+    function register_rsvp_event_handlers(rsvp_handler, word_color_input, wpm_input, text_container, topbar) {
+        word_color_input.addEventListener('change', event => {
+            rsvp_handler.word_color = event.target.value;
+        });
 
+        wpm_input.addEventListener('change', event => {
+            rsvp_handler.display_time_ms = 60000 / event.target.value;
+        });
+
+        window.onkeydown = event => {
+            switch (event.code) {
+                case 'Space': {
+                    rsvp_handler.word_display_element.classList.toggle('hidden');
+                    text_container.classList.toggle('hidden');
+                    topbar.classList.toggle('hidden');
+
+                    if (rsvp_handler.is_active)
+                        rsvp_handler.pause();
+                    else
+                        rsvp_handler.run();
+
+                    break;
+                }
+                case 'ArrowLeft': {
+                    rsvp_handler.rewind();
+                    break;
+                }
+                case 'ArrowRight': {
+                    rsvp_handler.forward();
+                    break;
+                }
+            }
+        };
     }
 
-    forward(num_steps = 8) {
-        return this._rewind_or_forward('nextElementSibling', num_steps);
+    function populate_text_container(text_container, text, word_element_tag_name) {
+        for (const word_segment of text.split(/\s+/)) {
+            const word_element = document.createElement(word_element_tag_name);
+            word_element.appendChild(document.createTextNode(`${word_segment} `));
+
+            text_container.appendChild(word_element);
+        }
     }
 
-    run() {
-        if (this.word_elements_iterator === null)
-            return void console.warn('The word elements iterator is `null`.');
-
-        const {value: iter_word_element, done} = this.word_elements_iterator.next();
-
-        this._uncolor_word_element(this._previous_element);
-
-        if (done)
-            return void this.set_display_text(this.done_text);
-
-        this._color_word_element(iter_word_element);
-        this.set_display_text(iter_word_element.textContent);
-
-        this._previous_element = iter_word_element;
-        // TODO: Fix the case where the delay is 0. Add an if statement?
-        this._current_timeout_id = window.setTimeout(this.run.bind(this), this.display_time_ms);
+    function set_default_input_values(input_value_entries) {
+        for (const [input, value] of input_value_entries) {
+            input.value = value;
+        }
     }
 
-    pause() {
-        window.clearTimeout(this._current_timeout_id);
-        this._current_timeout_id = null;
-    }
-}
+    return new Promise(async resolve => {
+        const {text} = await new Promise(resolve_message => {
+            function accept_message(message) {
+                browser.runtime.onMessage.removeListener(accept_message);
+                resolve_message(message);
+            }
 
-function populate_text_container(text_container, text, word_element_tag_name) {
-    for (const word_segment of text.split(/\s+/)) {
-        const word_element = document.createElement(word_element_tag_name);
-        word_element.appendChild(document.createTextNode(`${word_segment} `));
+            browser.runtime.onMessage.addListener(accept_message);
+            // Let the `executeScript` running this file in the background script return.
+            resolve();
+        });
 
-        text_container.appendChild(word_element);
-    }
-}
+        const config = await browser.runtime.sendMessage({message_type: 'get_config'});
 
-// TODO: Clean up.
-function accept_message(message) {
-    browser.runtime.onMessage.removeListener(accept_message);
+        const text_container = document.querySelector(config.text_container_selector);
+        const word_display_element = document.querySelector(config.word_display_element_selector);
+        const topbar = document.querySelector(config.topbar_selector);
+        const word_color_input = document.querySelector(config.word_color_input_selector);
+        const wpm_input = document.querySelector(config.wpm_input_selector);
 
-    const {text_container_selector, text, word_element_tag_name, word_display_element_selector, words_per_minute, word_color, done_text} = message;
+        set_default_input_values([
+            [word_color_input, config.word_color],
+            [wpm_input, config.words_per_minute]
+        ]);
 
-    const text_container = document.querySelector(text_container_selector);
-    const word_display_element = document.querySelector(word_display_element_selector);
-    const topbar = document.querySelector('.topbar');
-    const word_color_input = document.querySelector('.word_color_input');
-    const wpm_input = document.querySelector('.wpm_input');
+        populate_text_container(text_container, text, config.word_element_tag_name);
 
-    word_color_input.value = word_color;
-    wpm_input.value = words_per_minute;
+        const rsvp_handler = new RsvpHandler(
+            word_display_element, (60000 / config.words_per_minute),
+            config.word_color,
+            config.done_text
+        );
+        rsvp_handler.word_elements_iterator = make_sibling_iterator(text_container.firstElementChild);
 
-    populate_text_container(text_container, text, word_element_tag_name);
-
-    const rsvp_handler = new RsvpHandler(word_display_element, (60000 / words_per_minute), word_color, done_text);
-    rsvp_handler.word_elements_iterator = make_sibling_iterator(text_container.firstElementChild);
-
-    word_color_input.addEventListener('change', event => {
-        rsvp_handler.word_color = event.target.value;
+        register_rsvp_event_handlers(rsvp_handler, word_color_input, wpm_input, text_container, topbar);
     });
-
-    wpm_input.addEventListener('change', event => {
-        rsvp_handler.display_time_ms = 60000 / event.target.value;
-    });
-
-    window.onkeydown = event => {
-        switch (event.code) {
-            case 'Space': {
-                word_display_element.classList.toggle('hidden');
-                text_container.classList.toggle('hidden');
-                topbar.classList.toggle('hidden');
-
-                if (rsvp_handler.is_active)
-                    rsvp_handler.pause();
-                else
-                    rsvp_handler.run();
-
-                break;
-            }
-            case 'ArrowLeft': {
-                rsvp_handler.rewind();
-                break;
-            }
-            case 'ArrowRight': {
-                rsvp_handler.forward();
-                break;
-            }
-        }
-    };
-}
-
-browser.runtime.onMessage.addListener(accept_message);
+})();
